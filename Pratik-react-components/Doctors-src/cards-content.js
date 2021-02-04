19584b606e6c4054d25e51f9d583276393edb11e
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-02-05 01:07:53 +0530

    Update cards-content.js

--- Pratik-react-components/Doctors-src/cards-content.js
+++ Pratik-react-components/Doctors-src/cards-content.js
@@ -1,11 +1,11 @@
-import dummy from "./images/Dr.dummy";
+import dummy from "./images/Dr.dummy.webp";
 
 export const doctorCards = [
   {
     id: 1,
     status: "OPEN",
     img: dummy,
-    name: "Dr. Jhonny Sins",
+    name: "Dr. Dummy",
     degree: "Orthopedic Surgeon",
     address:
       "MAA SHANTI HOMOEO CARE, Ramdayalu Nagar - Hajipur Rd, near Muktinath Mandir, Lalgang Muzaffarpur, Bihar, 842002",
