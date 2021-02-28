f5a96ffec7fcefe157e7b5b87ff29c039c090b2a
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2021-02-28 23:35:46 +0530

    Some changes

--- src/Doctor/index.js
+++ src/Doctor/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import DoctorsList from "./app";
 import Detailcard from "../Doctors profile/detail-card";
-import ContactUs from "../Contact us/contactus";
 
 const Doctor = () => {
   return (
     <React.Fragment>
       <DoctorsList />
-      <ContactUs />
       <Detailcard />
     </React.Fragment>
   );
