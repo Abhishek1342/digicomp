e828a46544a443294e349ce3560ae3fb44f6ee77
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2021-08-08 22:43:33 +0530

    fixed data sending

--- backend/route/userauth.js
+++ backend/route/userauth.js
@@ -10,154 +10,145 @@ const router = express.Router();
 var { username, phone, email, password, cpassword } = 0;
 
 router.post("/user-register", async (req, res) => {
-        var { username, phone, email, password, cpassword } = req.body;
-        /*checking if user have entered all field or not*/
-        if (!username || !phone || !email || !password || !cpassword) {
-                return res
-                        .status(422)
-                        .json({ message: "please fill all data" });
-        }
+    var { username, phone, email, password, cpassword } = req.body;
+    /*checking if user have entered all field or not*/
+    if (!username || !phone || !email || !password || !cpassword) {
+        return res.status(422).json({ message: "please fill all data" });
+    }
 
-        try {
-                /*checking if already username exists or not*/
-                const usernameExist = await User.findOne({
-                        username: username,
+    try {
+        /*checking if already username exists or not*/
+        const usernameExist = await User.findOne({
+            username: username,
+        });
+
+        if (usernameExist) {
+            return res.status(422).json({ message: "username already exists" });
+        } else {
+            /*checking if already email exists or not*/
+            const emailExist = await User.findOne({ email: email });
+
+            if (emailExist) {
+                return res.status(422).json({
+                    message: "Email already exists",
                 });
+            }
+            if (password != cpassword) {
+                return res.status(422).json({
+                    error: "password and confirm password is not same",
+                });
+            } else {
+                var otpgenerator = Math.floor(Math.random() * 1000000);
+                otpgenerator = parseInt(otpgenerator);
 
-                if (usernameExist) {
-                        return res
-                                .status(422)
-                                .json({ message: "username already exists" });
-                } else {
-                        /*checking if already email exists or not*/
-                        const emailExist = await User.findOne({ email: email });
+                let transporter = require("../email/sendmail");
 
-                        if (emailExist) {
-                                return res.status(422).json({
-                                        message: "Email already exists",
-                                });
-                        }
-                        if (password != cpassword) {
-                                return res.status(422).json({
-                                        error: "password and confirm password is not same",
-                                });
-                        } else {
-                                var otpgenerator = Math.floor(
-                                        Math.random() * 1000000
-                                );
-                                otpgenerator = parseInt(otpgenerator);
+                let mailoptions = {
+                    from: "noreply.digicomp@gmail.com",
+                    to: email,
+                    subject: "Digicomp OTP for email varification",
+                    html:
+                        "<h3> Your one time password (otp) for registration in Digicomp is : </h3>" +
+                        "<h2>" +
+                        otpgenerator +
+                        "</h2>",
+                };
 
-                                let transporter = require("../email/sendmail");
-
-                                let mailoptions = {
-                                        from: "noreply.digicomp@gmail.com",
-                                        to: email,
-                                        subject: "Digicomp OTP for email varification",
-                                        html:
-                                                "<h3> Your one time password (otp) for registration in Digicomp is : </h3>" +
-                                                "<h2>" +
-                                                otpgenerator +
-                                                "</h2>",
-                                };
-
-                                transporter.sendMail(
-                                        mailoptions,
-                                        (err, data) => {
-                                                if (err) {
-                                                        console.log(
-                                                                "Mail not sent" +
-                                                                        err
-                                                        );
-                                                        res.status(400).json({
-                                                                error: "Mail not sent",
-                                                        });
-                                                } else {
-                                                        console.log(
-                                                                "Email sent"
-                                                        );
-                                                        res.status(200).json({
-                                                                message: "Mail sent",
-                                                        });
-                                                        res.redirect("/otp");
-                                                }
-                                        }
-                                );
-                        }
-                }
-        } catch (err) {
-                console.log("err register");
+                transporter.sendMail(mailoptions, (err, data) => {
+                    if (err) {
+                        console.log("Mail not sent" + err);
+                        res.status(400).json({
+                            error: "Mail not sent",
+                        });
+                    } else {
+                        console.log("Email sent");
+                        res.status(200).json({
+                            message: "Mail sent",
+                        });
+                        res.redirect("/otp");
+                    }
+                });
+            }
         }
-        return otpgenerator, { username, phone, email, password, cpassword };
+    } catch (err) {
+        console.log("err register");
+    }
+    return otpgenerator, { username, phone, email, password, cpassword };
 });
 
 router.post(
-        "/otp",
-        async (
-                res,
-                req,
-                otpgenerator,
-                { username, phone, email, password, cpassword }
-        ) => {
-                /*is new email and username then save the user*/
-                try {
-                        var otpuser = req.body;
-                        console.log(otpuser);
-                        if (otpuser != otpgenerator) {
-                                res.status(402).json({
-                                        message: "Otp incorrect",
-                                });
-                        } else {
-                                const newUser = new User({
-                                        username,
-                                        phone,
-                                        email,
-                                        password,
-                                        cpassword,
-                                });
+    "/otp",
+    async (
+        res,
+        req,
+        otpgenerator,
+        { username, phone, email, password, cpassword }
+    ) => {
+        /*is new email and username then save the user*/
+        try {
+            var otpuser = req.body;
+            console.log(otpuser);
+            if (otpuser != otpgenerator) {
+                res.status(402).json({
+                    message: "Otp incorrect",
+                });
+            } else {
+                const newUser = new User({
+                    username,
+                    phone,
+                    email,
+                    password,
+                    cpassword,
+                });
 
-                                const addUser = await newUser.save();
-                                if (addUser) {
-                                        console.log("successfull");
-                                        res.status(201).json({
-                                                message: "sucessfully added new user",
-                                        });
-                                }
-                        }
-                } catch (err) {
-                        console.log("err otp" + err);
+                const addUser = await newUser.save();
+                if (addUser) {
+                    console.log("successfull");
+                    res.status(201).json({
+                        message: "sucessfully added new user",
+                    });
                 }
+            }
+        } catch (err) {
+            console.log("err otp" + err);
         }
+    }
 );
 
 router.post("/user-login", async (req, res) => {
-        const { email, password } = req.body;
-        if (!email || !password) {
-                res.status(422).json({ message: "please enter all fields" });
+    const { email, password } = req.body;
+    console.log(req.body);
+    if (!email || !password) {
+        res.status(400).json({ message: "please enter all fields" });
+        return;
+    }
+    try {
+        const findUser = await User.findOne({ email: email });
+        if (findUser) {
+            const matchPassword = await bcrypt.compare(
+                password,
+                findUser.password
+            );
+            if (!matchPassword) {
+                res.status(422).json({
+                    message: "invalid credentials pass",
+                });
+                return;
+            } else {
+                res.status(200).json({
+                    message: "login sucessfull",
+                });
+                return;
+            }
+        } else {
+            res.status(422).json({
+                message: "invalid credentials email",
+            });
+            return;
         }
-        try {
-                const findUser = await User.findOne({ email: email });
-                if (findUser) {
-                        const matchPassword = await bcrypt.compare(
-                                password,
-                                findUser.password
-                        );
-                        if (!matchPassword) {
-                                res.status(422).json({
-                                        message: "invalid credentials pass",
-                                });
-                        } else {
-                                res.status(200).json({
-                                        message: "login sucessfull",
-                                });
-                        }
-                } else {
-                        res.status(422).json({
-                                message: "invalid credentials email",
-                        });
-                }
-        } catch (err) {
-                console.log(err);
-        }
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
--- client/src/Top-bar/Account/login.jsx
+++ client/src/Top-bar/Account/login.jsx
@@ -6,123 +6,123 @@ import InUp from "./inup";
 import SignUp from "./signup";
 import ForgotPassword from "./frgtpswd";
 import { modalStyle } from "../modalStyle";
+import axios from "axios";
 
 function Login() {
-        const [modalSignupIsOpen, setModalSignupIsOpen] = useState(false);
-        const [modalForgetIsOpen, setModalForgetIsOpen] = useState(false);
-        const [login, setLogin] = useState({ email: "", password: "" });
+    const [modalSignupIsOpen, setModalSignupIsOpen] = useState(false);
+    const [modalForgetIsOpen, setModalForgetIsOpen] = useState(false);
+    const [login, setLogin] = useState({ email: "", password: "" });
 
-        const handelInputs = (e) => {
-                let name, value;
-                name = e.target.name;
-                value = e.target.value;
-                setLogin({ ...value, [name]: value });
-        };
-        const submitLogin = async (e) => {
-                e.preventDefault();
-                const res = await fetch("http://localhost:8000/user-login", {
-                        method: "POST",
-                        headers: {
-                                "Content-Type": "application/json",
-                        },
-                        data: JSON.stringify(login),
-                });
-                console.log(res);
-        };
+    const handelInputs = (e) => {
+        let name, value;
+        name = e.target.name;
+        value = e.target.value;
+        setLogin((loginCurrent) => {
+            return { ...loginCurrent, [name]: value };
+        });
+    };
+    const submitLogin = async (e) => {
+        e.preventDefault();
+        try {
+            console.log(login);
+            const res = await axios.post(
+                "http://localhost:8000/user-login",
+                login
+            );
+            //     const res = await fetch("http://localhost:8000/user-login", {
+            //         method: "POST",
+            //         headers: {
+            //             "Content-Type": "application/json",
+            //         },
+            //         data: JSON.stringify(login),
+            //     });
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-        return (
-                <div className="popup-container">
-                        <InUp InUpTitle="Login" />
-                        <div className="login-partner">
-                                <a href="/#">
-                                        <img
-                                                src="https://img.icons8.com/color/38/000000/google-logo.png"
-                                                alt="Google icon"
-                                        />
-                                </a>
-                                <a href="/#">
-                                        <img
-                                                src="https://img.icons8.com/fluent-systems-filled/38/000000/mac-os.png"
-                                                alt="Aple icon"
-                                        />
-                                </a>
-                                <a href="/#">
-                                        <img
-                                                src="https://img.icons8.com/fluent/38/000000/facebook-new.png"
-                                                alt="FaceBook icon"
-                                        />
-                                </a>
-                        </div>
-                        <form className="form-container">
-                                <Textbox
-                                        type="Text"
-                                        id="username-login"
-                                        placeholder="Enter username"
-                                        onChange={handelInputs}
-                                        name="email"
-                                        value={login.email}
-                                />
-                                <PwdField
-                                        id="password-login"
-                                        placeholder="Enter Password"
-                                        onChange={handelInputs}
-                                        name="password"
-                                        value={login.password}
-                                />
-                                {/* <TnC id="trmncond" value="agree with Terms and Conditions" /> */}
-                                <Submit
-                                        id="submit-login"
-                                        value="Log in"
-                                        onClick={submitLogin}
-                                />
-                        </form>
-                        <div className="alternative-opt-container">
-                                <a
-                                        className="pointer-cursor"
-                                        onClick={() =>
-                                                setModalForgetIsOpen(true)
-                                        }
-                                >
-                                        Forgot password
-                                </a>
-                                <Modal
-                                        closeTimeoutMS={500}
-                                        isOpen={modalForgetIsOpen}
-                                        onRequestClose={() =>
-                                                setModalForgetIsOpen(false)
-                                        }
-                                        style={modalStyle}
-                                >
-                                        <ForgotPassword />
-                                </Modal>
-                                <p>
-                                        Don't have account? &nbsp;
-                                        <a
-                                                className="pointer-cursor"
-                                                onClick={() =>
-                                                        setModalSignupIsOpen(
-                                                                true
-                                                        )
-                                                }
-                                        >
-                                                Sign Up
-                                        </a>
-                                        <Modal
-                                                closeTimeoutMS={500}
-                                                isOpen={modalSignupIsOpen}
-                                                onRequestClose={() =>
-                                                        setModalSignupIsOpen(
-                                                                false
-                                                        )
-                                                }
-                                                style={modalStyle}
-                                        >
-                                                <SignUp />
-                                        </Modal>
-                                </p>
-                        </div>
-                </div>
-        );
+    return (
+        <div className="popup-container">
+            <InUp InUpTitle="Login" />
+            <div className="login-partner">
+                <a href="/#">
+                    <img
+                        src="https://img.icons8.com/color/38/000000/google-logo.png"
+                        alt="Google icon"
+                    />
+                </a>
+                <a href="/#">
+                    <img
+                        src="https://img.icons8.com/fluent-systems-filled/38/000000/mac-os.png"
+                        alt="Aple icon"
+                    />
+                </a>
+                <a href="/#">
+                    <img
+                        src="https://img.icons8.com/fluent/38/000000/facebook-new.png"
+                        alt="FaceBook icon"
+                    />
+                </a>
+            </div>
+            <form className="form-container">
+                <Textbox
+                    type="Text"
+                    id="username-login"
+                    placeholder="Enter username"
+                    onChange={handelInputs}
+                    name="email"
+                    value={login.email}
+                />
+                <PwdField
+                    id="password-login"
+                    placeholder="Enter Password"
+                    onChange={handelInputs}
+                    name="password"
+                    value={login.password}
+                />
+                {/* <TnC id="trmncond" value="agree with Terms and Conditions" /> */}
+                <Submit
+                    id="submit-login"
+                    value="Log in"
+                    onClick={submitLogin}
+                />
+            </form>
+            <div className="alternative-opt-container">
+                <a
+                    className="pointer-cursor"
+                    onClick={() => setModalForgetIsOpen(true)}
+                >
+                    Forgot password
+                </a>
+                <Modal
+                    closeTimeoutMS={500}
+                    isOpen={modalForgetIsOpen}
+                    onRequestClose={() => setModalForgetIsOpen(false)}
+                    style={modalStyle}
+                >
+                    <ForgotPassword />
+                </Modal>
+                <p>
+                    Don't have account? &nbsp;
+                    <a
+                        className="pointer-cursor"
+                        onClick={() => setModalSignupIsOpen(true)}
+                    >
+                        Sign Up
+                    </a>
+                    <Modal
+                        closeTimeoutMS={500}
+                        isOpen={modalSignupIsOpen}
+                        onRequestClose={() => setModalSignupIsOpen(false)}
+                        style={modalStyle}
+                    >
+                        <SignUp />
+                    </Modal>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
